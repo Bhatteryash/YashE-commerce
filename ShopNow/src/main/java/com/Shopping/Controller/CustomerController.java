a6f8d6a0fad3c96f267ac014cab178e93d712c2b
Yash Bhatter <yashbhatter.yb@gmail.com>
2022-11-28 10:52:00 +0530

    Services added

---
+++ ShopNow/src/main/java/com/Shopping/Controller/CustomerController.java
@@ -0,0 +1,62 @@
+package com.Shopping.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Shopping.Exceptions.CustomerException;
+import com.Shopping.Exceptions.LoginException;
+import com.Shopping.Model.Address;
+import com.Shopping.Model.Customer;
+import com.Shopping.Services.customerService;
+
+@RestController
+public class CustomerController {
+
+	@Autowired
+	private customerService cService;
+	
+	
+	@PostMapping("/addCustomer")
+	public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) throws CustomerException{
+		
+		Customer newCustomer = cService.AddCustomer(customer);
+		
+		return new ResponseEntity<Customer>(newCustomer,HttpStatus.ACCEPTED);
+	}
+	
+	@PutMapping("/UpdateCustomer")
+	public ResponseEntity<Customer> updateCustomer(@RequestParam String key,@RequestBody  Customer customer) throws CustomerException, LoginException{
+		
+		Customer updatedcustomer= cService.updateCustomer(key, customer);
+		
+		return new ResponseEntity<Customer>(updatedcustomer,HttpStatus.ACCEPTED);
+		
+	}
+	
+	@PutMapping("/AddAddress")
+	public ResponseEntity<Address> addAddress(@RequestBody Address address,String key,Integer customerId) throws CustomerException, LoginException{
+		
+		Address addr= cService.AddAddress(address, key, customerId);
+		
+		return new ResponseEntity<Address>(addr,HttpStatus.ACCEPTED);
+	}
+	
+	
+	@DeleteMapping("/deleteCustomer")
+	public ResponseEntity<String> deleteCustomer(Integer customerId ,String key) throws CustomerException, LoginException{
+		
+		String message= cService.deleteCustomer(customerId, key);
+		
+		return new ResponseEntity<String>(message,HttpStatus.ACCEPTED);
+	}
+	
+	
+	
+}
